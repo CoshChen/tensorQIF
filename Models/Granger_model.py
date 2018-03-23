08e57aca05f66f429386cb5cb69aa49db3c078f5
CoshChen <cosh.chen@gmail.com>
2018-03-22 22:13:58 -0400

    Add files via upload

--- Models/Granger_model.py
+++ Models/Granger_model.py
@@ -17,20 +17,25 @@ import os
 import csv
 import Utils
 
-data_struct = '2000_5x5x5'
+data_struct = '1000_5x5x5_Exch08'
 data_dir = '../SynData/' + data_struct
-report_file = '../SynData/result/'+data_struct+'/Granger_LASSO_'+data_struct+'.csv'
-train_size = 1500 # actual training size = train_size*(T-tau)
+result_dir = '../SynData/result/'+data_struct
+report_file = result_dir+'/Granger_LASSO_'+data_struct+'.csv'
+train_size = 800 # actual training size = train_size*(T-tau)
 dataset_num = 100
 
+if not os.path.exists(result_dir):
+    os.makedirs(result_dir)
+
+
 with open(report_file, 'w') as csvfile:
-    fieldnames = ['Data #', 'Granger Training MSE', 'Granger Test MSE', 'LASSO Granger Training MSE','LASSO Granger Test MSE']
+    fieldnames = ['Data #', 'Granger Training MSE', 'Granger Test MSE', 'LASSO Granger Training MSE','LASSO Granger Test MSE', 'Lambda']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     
     for data_id in range(dataset_num):
         data = data_dir + '/'+data_struct+'_' + str(data_id) + '.npz'
-        outfile_dir = '../SynData/result/'+data_struct+'/'+data_struct+'_' + str(data_id)
+        outfile_dir = result_dir+'/'+data_struct+'_' + str(data_id)
         trained_result = outfile_dir + '/Granger_trainedW.npz'
         trained_result_LASSO = outfile_dir + '/LASSO_Granger_trainedW.npz'
         
@@ -54,7 +59,7 @@ with open(report_file, 'w') as csvfile:
         y_test = y_vect[train_size*(T-tau):]
         
         # Linear Regresssion
-        gr = linear_model.LinearRegression()
+        gr = linear_model.LinearRegression(fit_intercept=False)
         gr.fit(X_train, y_train)
         
         W = gr.coef_.reshape([(tau+1),d1,d2])
@@ -66,7 +71,23 @@ with open(report_file, 'w') as csvfile:
         gr_test_MSE = metrics.mean_squared_error(y_test, gr_y_pred_test)       
         
         # LASSO
-        gr_lasso = linear_model.Lasso(alpha=1.0)
+        # Choose Lambda
+        scale = 0.01
+        cv_train_size = int((train_size/2)*(T-tau))
+        final_lam = 0.0
+        vali_MSE = 2*gr_test_MSE
+        
+        for lam in range(1,6):
+            gr_lasso = linear_model.Lasso(fit_intercept=False, alpha=lam*scale)
+            gr_lasso.fit(X_train[:cv_train_size, :], y_train[:cv_train_size])
+            gr_lasso_y_vali = gr_lasso.predict(X_train[cv_train_size:, :])
+            
+            if metrics.mean_squared_error(y_train[cv_train_size:], gr_lasso_y_vali) < vali_MSE:
+                final_lam = lam*scale
+                vali_MSE = metrics.mean_squared_error(y_train[cv_train_size:], gr_lasso_y_vali)
+        
+        # Fit Lasso Granger for all training data
+        gr_lasso = linear_model.Lasso(fit_intercept=False, alpha=final_lam)
         gr_lasso.fit(X_train, y_train)
         
         W_lasso = gr_lasso.coef_.reshape([(tau+1),d1,d2])
@@ -82,6 +103,7 @@ with open(report_file, 'w') as csvfile:
                          'Granger Training MSE': gr_train_MSE, 
                          'Granger Test MSE': gr_test_MSE,
                          'LASSO Granger Training MSE': gr_lasso_train_MSE,
-                         'LASSO Granger Test MSE': gr_lasso_test_MSE})
+                         'LASSO Granger Test MSE': gr_lasso_test_MSE,
+                         'Lambda': final_lam})
 
 
