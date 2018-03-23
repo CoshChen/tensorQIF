2b377ef7782a0605fa3e546fb4205d7b633c96a3
CoshChen <cosh.chen@gmail.com>
2018-03-22 22:13:33 -0400

    Add files via upload

--- DataGenerator.py
+++ DataGenerator.py
@@ -33,34 +33,34 @@ import Utils
 
 ############### Input Variables ###############
 dataset_num = 100 # number of datasets needed
-m = 500 # sample size
+m = 1000 # sample size
 # Sample size setting ref:
 # https://stats.stackexchange.com/questions/60622/why-is-a-sample-covariance-matrix-singular-when-sample-size-is-less-than-number
 
 T = 20 # number of time points
 tau = 4
 d1 = 5
-d2 = 7
+d2 = 5
 
 # None zero indicies (Patterns in Ws)
 pattern_1 = [0] # time; numbers in range(tau)
 pattern_2 = [0] # d1; numbers in range(d1)
 pattern_3 = [0] #d2; numbers in range(d2)
 
-alpha = 0.6 # Error term has AR(1, alpha)
+alpha = 0.8 # Error term
 ###############################################
 
 
 def get_X(m, T, d1, d2, mu, sigma):
     return np.random.normal(mu, sigma, size=(m, T, d1, d2))
 
-def get_y(X, W, T, tau, alpha):
+def get_y(X, W, T, tau, alpha, cov_struct='AR1'):
     '''
     @param X: numpy array [m, T, d1, d2]
     @param W: numpy array [(tau+1), d1, d2]
     @param T: number of time points in dataset 
     @param tau: parameter in range(T-1)
-    @param alpha: AR(1) correlation parameter
+    @param alpha: correlation parameter
     @return numpy array [m, T-tau]
     '''
     
@@ -72,15 +72,19 @@ def get_y(X, W, T, tau, alpha):
     
     mean = np.zeros(T-tau)
     cov = np.eye(T-tau)
-    for diff in range(1, T-tau):
-        cov += (math.pow(alpha,diff)*np.eye(T-tau, k=diff))
-        cov += (math.pow(alpha,diff)*np.eye(T-tau, k=-diff))
-    # cov = AR(alpha)
+    
+    if cov_struct == 'AR1':
+        for diff in range(1, T-tau):
+            cov += (math.pow(alpha,diff)*np.eye(T-tau, k=diff))
+            cov += (math.pow(alpha,diff)*np.eye(T-tau, k=-diff))
+    
+    elif cov_struct == 'exchangeable':
+        cov += alpha*(np.ones([T-tau, T-tau]) - np.eye(T-tau))
     
     s = np.random.multivariate_normal(mean, cov, size = (m)) # [m, T-tau]
     
     for t in range(T-tau):
-        y[:,t] = np.squeeze(np.matmul(X_repeat[:, t, :], W_vect)) + np.sin(t)
+        y[:,t] = np.squeeze(np.matmul(X_repeat[:, t, :], W_vect))
     
     y += s
     return y    
@@ -90,7 +94,7 @@ def get_y(X, W, T, tau, alpha):
 '''
 Generate Datasets
 '''
-data_struct = str(m) +'_'+ str(tau+1)+'x'+str(d1)+'x'+str(d2)
+data_struct = str(m) +'_'+ str(tau+1)+'x'+str(d1)+'x'+str(d2)+'_Exch08'
 data_dir = './SynData/' + data_struct
 mat_dir = data_dir + '/matFiles'
 
@@ -114,9 +118,16 @@ for i in range(dataset_num):
     W_list[1][:, pattern_2, :] = np.random.uniform(low=-9.0, high=9.0, size=(tau+1, len(pattern_2), d2))
     W_list[2][:, :, pattern_3] = np.random.uniform(low=-9.0, high=9.0, size=(tau+1, d1, len(pattern_3)))
     
+    # To generate very sparse coefficients
+    #for _ in range(2):
+    #    mask= np.random.randint(2, size=(tau+1, d1, d2))
+    #    W_list[0] = np.multiply(W_list[0], mask)
+    #    W_list[1] = np.multiply(W_list[1], mask)
+    #    W_list[2] = np.multiply(W_list[2], mask)
+    
     W = W_list[0] + W_list[1] + W_list[2]
     X = get_X(m, T, d1, d2, 0.0, 2.0) # [m, T, d1, d2]
-    y = get_y(X, W, T, tau, alpha) # [m, T-tau]
+    y = get_y(X, W, T, tau, alpha, 'exchangeable') # [m, T-tau]
     
     np.savez(outfile, X=X, y=y, W1=W_list[0], W2=W_list[1], W3=W_list[2])
     scipy.io.savemat(mat_file, dict(X=X, y=y, W1=W_list[0], W2=W_list[1], W3=W_list[2]))
