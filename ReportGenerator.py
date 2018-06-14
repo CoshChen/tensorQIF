e23a22cd3811484664d1ba2eb5f8086e7ddf0bd1
CoshChen <cosh.chen@gmail.com>
2018-06-14 13:11:51 -0400

    Add files via upload

--- ReportGenerator.py
+++ ReportGenerator.py
@@ -14,8 +14,10 @@ import Utils
 ######## Input Variables ########
 model = 'tf_tensorQIF'
 dataset_num = 100
-data_struct = '500_5x5x5'
-train_size = 400
+testset_num = 3
+data_struct = '1000_3x2x2_AR08_c4'
+test_data_struct = '10000_3x2x2_AR08_c4_test'
+sample_size = 300
 #################################
 
 single_W = True
@@ -23,11 +25,12 @@ if model == 'tf_tensorQIF' or model == 'tensorQIF':
     single_W = False
 
 data_dir = './SynData/' + data_struct
-result_dir = './SynData/result/' + str(data_struct)
+test_data_dir = './SynData/TestSet/' + str(test_data_struct)
+result_dir = './SynData/result/' + str(data_struct) + '/m' + str(sample_size)
 report_file = result_dir + '/' + model + '_' + data_struct + '.csv'
 
 with open(report_file, 'w') as csvfile:
-    fieldnames = ['Data #', model + ' Training MSE', model + ' Test MSE']
+    fieldnames = ['Data #','Tr MSE','Va MSE'] + ['TePr MSE '+str(t) for t in range(testset_num)]+['TeTy MSE '+str(t) for t in range(testset_num)]+['W RMSE','Lambda']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     
@@ -35,43 +38,77 @@ with open(report_file, 'w') as csvfile:
         data = data_dir+'/'+data_struct+'_'+str(data_id)+'.npz'
         trained_result = result_dir +'/'+ data_struct +'_'+ str(data_id)+'/'+model+'_trainedW.npz'
         
+        row = []
+        
         if not os.path.exists(trained_result):
             print("No trained result for data # " + str(data_id))
             continue
         
         npzfile = np.load(data)
-        X_train = npzfile['X'][:train_size, :,:,:]
-        X_test = npzfile['X'][train_size:, :,:,:]
-        y_train = npzfile['y'][:train_size, :]
-        y_test = npzfile['y'][train_size:, :]
+        
+        cv_train_size = int(sample_size*0.8)
+        X_train = npzfile['X'][:cv_train_size, :,:,:]
+        X_cv = npzfile['X'][cv_train_size:sample_size, :,:,:]
+        y_train = npzfile['y'][:cv_train_size, :]
+        y_cv = npzfile['y'][cv_train_size:sample_size, :]
+        
+        W_true = npzfile['W1'] + npzfile['W2'] + npzfile['W3']
+        lam = -1
         
         if single_W:
             W_trained = np.load(trained_result)['W']
         else:
             Ws = np.load(trained_result) # tensorQIF
+            lam = Ws['lam']
             W_trained = Ws['W1'] + Ws['W2'] + Ws['W3']
         
         
         # Calculate MSE
         _, T, d1, d2 = X_train.shape
-        test_size = y_test.shape[0]
+        cv_size = sample_size - cv_train_size
         
-        if model in ['tensorQIF', 'tf_tensorQIF', 'Granger', 'LASSO_Granger']:
-            tau = T - y_test.shape[1]
+        if model in ['tensorQIF', 'tf_tensorQIF', 'LASSO_Granger']:
+            tau = T - y_cv.shape[1]
         else:
             tau = 0
         
-        X_train_flat = np.reshape(Utils.get_X_repeat(X_train,tau), [train_size*(T-tau), (tau+1)*d1*d2])
-        X_test_flat = np.reshape(Utils.get_X_repeat(X_test,tau), [test_size*(T-tau), (tau+1)*d1*d2])
+        X_train_flat = np.reshape(Utils.get_X_repeat(X_train,tau), [cv_train_size*(T-tau), (tau+1)*d1*d2])
+        X_cv_flat = np.reshape(Utils.get_X_repeat(X_cv,tau), [cv_size*(T-tau), (tau+1)*d1*d2])
         W_vect = np.reshape(W_trained, [(tau+1)*d1*d2, 1])
         
-        y_pred_train = np.reshape(np.matmul(X_train_flat, W_vect), [train_size,T-tau])[:, :y_train.shape[1]]
-        y_pred_test = np.reshape(np.matmul(X_test_flat, W_vect), [test_size,T-tau])[:, :y_test.shape[1]]
+        y_pred_train = np.reshape(np.matmul(X_train_flat, W_vect), [cv_train_size,T-tau])[:, :y_train.shape[1]]
+        y_pred_cv = np.reshape(np.matmul(X_cv_flat, W_vect), [cv_size,T-tau])[:, :y_cv.shape[1]]
         train_MSE = np.mean(np.square(y_train - y_pred_train))
-        test_MSE = np.mean(np.square(y_test - y_pred_test))
+        vali_MSE = np.mean(np.square(y_cv - y_pred_cv))
+        
+        W_RMSE = np.sqrt(np.mean(np.square(W_true - W_trained)))
+        
+        row.append(data_id)
+        row.append(train_MSE)
+        row.append(vali_MSE)
+        
+        TePr = []
+        TeTy = []
+        for test_id in range(testset_num):
+            test_file = np.load(test_data_dir+'/'+test_data_struct+'_'+str(test_id)+'.npz')
+            
+            X_test = test_file['X']
+            y_test = test_file['y']
+            W_true_vect = np.reshape(W_true, [(tau+1)*d1*d2, 1])
+            test_size, _, _, _ = X_test.shape
+            
+            X_test_flat = np.reshape(Utils.get_X_repeat(X_test,tau), [test_size*(T-tau), (tau+1)*d1*d2])
+            y_pred_test = np.reshape(np.matmul(X_test_flat, W_vect), [test_size,T-tau])[:, :y_test.shape[1]]
+            
+            TePr.append(np.mean(np.square(y_pred_test - y_test)))
+            TeTy.append(np.mean(np.square(np.matmul(X_test_flat, W_vect - W_true_vect))))
+        
+        row += TePr
+        row += TeTy
+        
+        row.append(W_RMSE)
+        row.append(lam)
         
         # write in CSV file
-        writer.writerow({'Data #': data_id, 
-                         model + ' Training MSE': train_MSE,
-                         model + ' Test MSE': test_MSE})
+        writer.writerow({fieldnames[col]: row[col] for col in range(len(fieldnames))})
 
